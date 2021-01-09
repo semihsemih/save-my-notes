8603577a9a5f32318bee2cdd2dc3b1767c8f7da4
Semih Arslan <semihsemih@me.com>
2021-01-09 18:46:16 +0300

    fixed users schema email column as unique

--- models/user.go
+++ models/user.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	gorm.Model
 	UUID     uuid.UUID `json:"uuid"`
-	Email    string    `json:"email" validate:"required,email"`
+	Email    string    `json:"email" gorm:"unique" validate:"required,email"`
 	Password string    `json:"password" validate:"required,min=6"`
 	Status   bool      `json:"status"`
 	Lists    []List    `json:"lists"`
