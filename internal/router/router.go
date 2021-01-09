7f106819d5d387b7a36cbfddfce66289ca033c43
Semih Arslan <semihsemih@me.com>
2021-01-09 12:00:23 +0300

    Add unsigned integer pattern to id variables in routers

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,20 +11,20 @@ func Init(controller *controllers.Controller) *mux.Router {
 	router.HandleFunc("/api/user/signup", controller.Signup()).Methods("POST")
 	router.HandleFunc("/api/user/login", controller.Login()).Methods("POST")
 	router.HandleFunc("/api/user/activation/{token}", controller.AccountActivation()).Methods("GET")
-	router.HandleFunc("/api/user/{id}", controller.TokenVerifyMiddleware(controller.GetUser())).Methods("GET")
+	router.HandleFunc("/api/user/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.GetUser())).Methods("GET")
 
 
 	/* List Action Routes */
 	router.HandleFunc("/api/list", controller.TokenVerifyMiddleware(controller.InsertList())).Methods("POST")
-	router.HandleFunc("/api/list/{id}", controller.TokenVerifyMiddleware(controller.GetList())).Methods("GET")
-	router.HandleFunc("/api/list/{id}", controller.TokenVerifyMiddleware(controller.UpdateList())).Methods("PUT")
-	router.HandleFunc("/api/list/{id}", controller.TokenVerifyMiddleware(controller.DeleteList())).Methods("DELETE")
+	router.HandleFunc("/api/list/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.GetList())).Methods("GET")
+	router.HandleFunc("/api/list/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.UpdateList())).Methods("PUT")
+	router.HandleFunc("/api/list/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.DeleteList())).Methods("DELETE")
 
 	/* Note Action Routes */
 	router.HandleFunc("/api/note", controller.TokenVerifyMiddleware(controller.InsertNote())).Methods("POST")
-	router.HandleFunc("/api/note/{id}", controller.TokenVerifyMiddleware(controller.GetNote())).Methods("GET")
-	router.HandleFunc("/api/note/{id}", controller.TokenVerifyMiddleware(controller.UpdateNote())).Methods("PUT")
-	router.HandleFunc("/api/note/{id}", controller.TokenVerifyMiddleware(controller.DeleteNote())).Methods("DELETE")
+	router.HandleFunc("/api/note/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.GetNote())).Methods("GET")
+	router.HandleFunc("/api/note/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.UpdateNote())).Methods("PUT")
+	router.HandleFunc("/api/note/{id:[0-9]+}", controller.TokenVerifyMiddleware(controller.DeleteNote())).Methods("DELETE")
 
 	return router
 }
